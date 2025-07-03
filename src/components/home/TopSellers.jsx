e5f906bd03e88da2fb21a21ab2460f9f92d29ba2
Alphonse <kubalpha19@gmail.com>
2025-07-03 02:04:38 -0700

    collect top sellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,38 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const TopSellers = () => {
+  const [dataFetched, setDataFetched] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Add optional delay (500ms)
+    const timer = setTimeout(() => {
+      const fetchTopSellers = async () => {
+        try {
+          const { data } = await axios.get(
+            "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+          );
+          setDataFetched(data);
+        } catch (error) {
+          console.log("Error fetching data:", error);
+        } finally {
+          setLoading(false); // Always stop loading
+        }
+      };
+
+      fetchTopSellers();
+    }, 5000);
+
+    return () => clearTimeout(timer); // Clean up timer
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Top Sellers</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
+          <div className="col-lg-12 text-center">
+            <h2>Top Sellers</h2>
+            <div className="small-border bg-color-2"></div>
           </div>
+
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading
+                ? Array.from({ length: 12 }).map((_, i) => (
+                    <li key={i}>
+                      <div className="author_list_pp">
+                        <Skeleton circle height={50} width={50} />
+                      </div>
+                      <div className="author_list_info">
+                        <Skeleton width={120} height={20} />
+                        <Skeleton width={80} height={15} />
+                      </div>
+                    </li>
+                  ))
+                : dataFetched.map((user) => (
+                    <li key={user.id}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${user.id}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={user.authorImage}
+                            alt={user.authorName}
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${user.id}`}>{user.authorName}</Link>
+                        <span>{user.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,90 +1,162 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ItemDetails = () => {
+  const { nftId } = useParams();
+  const [nftData, setNftData] = useState(null);
+  const isLoading = !nftData;
+ let timeoutId;
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+        );
+     timeoutId = setTimeout(() => {
+        setNftData(data);
+        window.scrollTo(0, 0);
+      }, 2000);
+    } catch (error) {
+      console.error("Error fetching NFT item:", error);
+    }
+  };
+    fetchData();
+  }, [nftId]);
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
+              {/* Left Image Section */}
               <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
+                {isLoading ? (
+                  <Skeleton height={400} width={"100%"} />
+                ) : (
+                  <img
+                    src={nftData.nftImage}
+                    className="img-fluid img-rounded mb-sm-30 nft-image"
+                    alt={nftData.title}
+                  />
+                )}
               </div>
+
+              {/* Right Info Section */}
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>
+                    {isLoading ? (
+                      <Skeleton width={300} />
+                    ) : (
+                      `${nftData.title} #${nftData.tag}`
+                    )}
+                  </h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
+                      <i className="fa fa-eye"></i>{" "}
+                      {isLoading ? <Skeleton width={40} /> : nftData.views}
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
+                      <i className="fa fa-heart"></i>{" "}
+                      {isLoading ? <Skeleton width={40} /> : nftData.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+
+                  <p>{isLoading ? <Skeleton count={4} /> : nftData.description}</p>
+
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {isLoading ? (
+                            <Skeleton circle={true} height={50} width={50} />
+                          ) : (
+                            <Link to={`/author/${nftData.ownerId || ""}`}>
+                              <img
+                                className="lazy"
+                                src={nftData.creatorImage}
+                                alt={nftData.owner}
+                              />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          {isLoading ? (
+                            <Skeleton width={100} />
+                          ) : (
+                            <Link to={`/author/${nftData.ownerId || ""}`}>
+                              {nftData.owner}
+                            </Link>
+                          )}
+                          {!isLoading && (
+                            <div>
+                              <i style={{ fontSize: "20px" }}>
+                                <FontAwesomeIcon
+
+                                  className="fa-arrow-left"
+                                />
+                              </i>
+                            </div>
+                          )}
                         </div>
                       </div>
                     </div>
-                    <div></div>
                   </div>
+
                   <div className="de_tab tab_simple">
                     <div className="de_tab_content">
+                      <br />
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {isLoading ? (
+                            <Skeleton circle={true} height={50} width={50} />
+                          ) : (
+                            <Link to={`/author/${nftData.creatorId || ""}`}>
+                              <img src={nftData.ownerImage} alt="" />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          {isLoading ? (
+                            <Skeleton width={100} />
+                          ) : (
+                            <Link to={`/author/${nftData.creatorId || ""}`}>
+                              {nftData.creatorName || "Unknown"}
+                            </Link>
+                          )}
                         </div>
                       </div>
                     </div>
+
                     <div className="spacer-40"></div>
+
                     <h6>Price</h6>
-                    <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                    <div className="nft-item-price d-flex align-items-center">
+                      {isLoading ? (
+                        <Skeleton width={80} height={25} />
+                      ) : (
+                        <>
+                          <img src={EthImage} alt="ETH" />
+                          <span>{nftData.price} ETH</span>
+                        </>
+                      )}
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
+              </div> {/* end .col-md-6 */}
+            </div> {/* end .row */}
+          </div> {/* end .container */}
         </section>
       </div>
     </div>
